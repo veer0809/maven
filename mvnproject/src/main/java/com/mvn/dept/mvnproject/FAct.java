935adecfd9a7ae3fea03d993bb8df001c0b8139f
veer <veer@gmail.com>
2018-09-28 15:48:17 +0530

    commit

---
+++ mvnproject/src/main/java/com/mvn/dept/mvnproject/FAct.java
@@ -0,0 +1,17 @@
+package com.mvn.dept.mvnproject;
+
+public class FAct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n=6,fact=1;
+			for(n=1;n<=1;n++)
+			{
+			 fact=fact*n;
+			
+			}
+			System.out.println("factorial is :" +fact);
+	}
+
+}
